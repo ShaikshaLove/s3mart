e461d04ea9d38f1d53f0341b887e51a3013990a2
ShaikshaLove <44459273+ShaikshaLove@users.noreply.github.com>
2024-10-24 12:25:44 +0530

    it's an ecom app

---
+++ src/main/java/io/shasoft/s3mart/S3martApplication.java
@@ -0,0 +1,13 @@
+package io.shasoft.s3mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3martApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3martApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/shasoft/s3mart/S3martApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shasoft.s3mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3martApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
